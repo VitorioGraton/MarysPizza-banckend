324bb40df5a382dff582c7c3cf6ba191cc137a21
monts <montemurro.matheus@gmail.com>
2023-08-11 12:49:04 -0300

    first commit

---
+++ src/main/java/com/example/MarysPizza/MarysPizzaApplication.java
@@ -0,0 +1,13 @@
+package com.example.MarysPizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarysPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarysPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MarysPizza/MarysPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MarysPizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarysPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
