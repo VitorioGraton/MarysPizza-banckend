371fbaff15f5fe8ebc97e35bca9b89f528b18e08
montemurro19 <montemurro.matheus@gmail.com>
2023-09-04 18:19:00 -0300

    Co-authored-by: VitorioGraton <VitorioGraton@users.noreply.github.com>

--- src/main/java/com/example/MarysPizza/repositories/PedidoRepository.java
+++ src/main/java/com/example/MarysPizza/repositories/PedidoRepository.java
@@ -2,12 +2,15 @@ package com.example.MarysPizza.repositories;
 
 import com.example.MarysPizza.models.Pedido;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {   
     
-    Page<Pedido> findByDescricaoContaining(String busca, Pageable pageable);
+    Page<Pedido> findByObservacaoContaining(String observacao, Pageable pageable);
 
+    Optional<Pedido> findPedidoByIdPedido(Long idPedido);
 }
